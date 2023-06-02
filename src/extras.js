a0cdf46891d190449f6221e0c5eb7cbdd673fb14
Idle <651462+reonZ@users.noreply.github.com>
2023-06-02 21:38:35 +0200

    added rest & dailies in extras

--- src/extras.js
+++ src/extras.js
@@ -9,6 +9,7 @@ export async function getExtrasData(actor) {
             selected: initiative.statistic,
             skills: SKILLS_SLUGS.map(slug => ({ slug, label: getSkillLabel(slug) })),
         },
+        hasDailies: game.modules.get('pf2e-dailies').active,
     }
 }
 
@@ -22,5 +23,11 @@ export function addExtrasListeners(el, actor) {
     el.find('[data-action=roll-initiative]').on('click', async event => {
         event.preventDefault()
         await actor.initiative.roll({ event })
+    })
+
+    el.find('[data-action=prepare-dailies]').on('click', async event => {
+        event.preventDefault()
+        const dailies = game.modules.get('pf2e-dailies')
+        if (dailies.active) dailies.api.openDailiesInterface(actor)
     })
 }
