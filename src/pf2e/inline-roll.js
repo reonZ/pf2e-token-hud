2614041e6906deeb2d2a1c60dd6afea431f4bd4b
Idle <651462+reonZ@users.noreply.github.com>
2023-07-26 16:17:06 +0200

    updated inline roll listeners

--- src/pf2e/inline-roll.js
+++ src/pf2e/inline-roll.js
@@ -1,7 +1,6 @@
-import { calculateDC } from './dc.js'
 import { htmlClosest, htmlQueryAll } from './dom.js'
+import { tupleHasValue } from './misc.js'
 import { eventToRollParams } from './scripts.js'
-import { calculateDegreeOfSuccess } from './success.js'
 
 const inlineSelector = ['action', 'check', 'effect-area'].map(keyword => `[data-pf2-${keyword}]`).join(',')
 
@@ -24,10 +23,20 @@ function injectRepostElement(links, actor) {
 
         const newButton = document.createElement('i')
         const icon = link.parentElement?.dataset?.pf2Checkgroup !== undefined ? 'fa-comment-alt-dots' : 'fa-comment-alt'
-        newButton.classList.add('fas', icon)
-        newButton.setAttribute('data-pf2-repost', '')
-        newButton.setAttribute('title', game.i18n.localize('PF2E.Repost'))
+        newButton.classList.add('fa-solid', icon)
+        newButton.dataset.pf2Repost = ''
+        newButton.title = game.i18n.localize('PF2E.Repost')
         link.appendChild(newButton)
+
+        newButton.addEventListener('click', event => {
+            event.stopPropagation()
+
+            const target = event.target
+            if (!(target instanceof HTMLElement)) return
+
+            const parent = target?.parentElement
+            if (parent) repostAction(parent, actor)
+        })
     }
 }
 
@@ -50,7 +59,7 @@ function repostAction(target, actor = null) {
         return
     }
 
-    const defaultVisibility = actor?.hasPlayerOwner ? 'all' : 'gm'
+    const defaultVisibility = (actor ?? actor)?.hasPlayerOwner ? 'all' : 'gm'
     const content = (() => {
         if (target.parentElement?.dataset?.pf2Checkgroup !== undefined) {
             const content = htmlQueryAll(target.parentElement, inlineSelector)
@@ -64,13 +73,9 @@ function repostAction(target, actor = null) {
     })()
 
     const ChatMessagePF2e = CONFIG.ChatMessage.documentClass
-    const speaker =
-        actor instanceof Actor
-            ? ChatMessagePF2e.getSpeaker({
-                  actor: actor,
-                  token: actor.token ?? actor.getActiveTokens(false, true).shift(),
-              })
-            : ChatMessagePF2e.getSpeaker()
+    const speaker = actor
+        ? ChatMessagePF2e.getSpeaker({ actor, token: actor.getActiveTokens(false, true).shift() })
+        : ChatMessagePF2e.getSpeaker()
 
     const message = game.messages.get(htmlClosest(target, '[data-message-id]')?.dataset.messageId ?? '')
     const flags = message?.flags.pf2e.origin ? { pf2e: { origin: deepClone(message.flags.pf2e.origin) } } : {}
@@ -90,105 +95,128 @@ function repostAction(target, actor = null) {
 export function listenInlineRoll($html, actor) {
     const html = $html instanceof HTMLElement ? $html : $html[0]
 
-    const links = htmlQueryAll(html, inlineSelector).filter(l => l.nodeName === 'SPAN')
-    injectRepostElement(links, actor)
+    const links = htmlQueryAll(html, inlineSelector).filter(l => ['A', 'SPAN'].includes(l.nodeName))
 
+    injectRepostElement(links, actor)
     flavorDamageRolls(html, actor)
 
-    htmlQueryAll(html, 'i[data-pf2-repost]').forEach(btn =>
-        btn.addEventListener('click', event => {
-            const target = event.target
-            if (!(target instanceof HTMLElement)) return
-            const parent = target?.parentElement
-            if (parent) repostAction(parent, actor)
-            event.stopPropagation()
-        })
-    )
-
-    const $links = $(links)
-    $links.filter('[data-pf2-action]').on('click', event => {
-        const $target = $(event.currentTarget)
-        const { pf2Action, pf2Glyph, pf2Variant, pf2Dc, pf2ShowDc, pf2Skill } = $target[0]?.dataset ?? {}
-        const action = game.pf2e.actions[pf2Action ? game.pf2e.system.sluggify(pf2Action, { camel: 'dromedary' }) : '']
-        const visibility = pf2ShowDc ?? 'all'
-        if (pf2Action && action) {
-            action({
-                event,
-                glyph: pf2Glyph,
-                variant: pf2Variant,
-                difficultyClass: pf2Dc ? { scope: 'check', value: Number(pf2Dc) || 0, visibility } : undefined,
-                skill: pf2Skill,
-                actors: [actor],
-            })
-        } else {
-            console.warn(`PF2e System | Skip executing unknown action '${pf2Action}'`)
-        }
-    })
-
-    $links.filter('[data-pf2-check]').on('click', async event => {
-        const { pf2Check, pf2Dc, pf2Traits, pf2Label, pf2Adjustment } = event.currentTarget.dataset
-        const parsedTraits = pf2Traits
-            ?.split(',')
-            .map(trait => trait.trim())
-            .filter(trait => !!trait)
-        const eventRollParams = eventToRollParams(event)
-
-        switch (pf2Check) {
-            case 'flat': {
-                const dc = Number.isInteger(Number(pf2Dc)) ? { label: pf2Label, value: Number(pf2Dc) } : null
-                const roll = await new Roll('1d20').evaluate({ async: true })
-                const rollTotal = roll.total
-                const dieResult = roll.dice[0].total
-                const success = calculateDegreeOfSuccess(rollTotal, dieResult, dc.value)
-                const degree = {
-                    value: success,
-                    unadjusted: success,
-                    adjustment: null,
-                    dieResult,
-                    rollTotal,
-                    dc,
-                }
-                const flavor = (await game.pf2e.Check.createResultFlavor({ degree })).outerHTML
-                roll.toMessage({ flavor })
-                break
+    for (const link of links.filter(l => l.dataset.pf2Action)) {
+        const { pf2Action, pf2Glyph, pf2Variant, pf2Dc, pf2ShowDc, pf2Skill } = link.dataset
+        link.addEventListener('click', event => {
+            const action = game.pf2e.actions[pf2Action ? game.pf2e.system.sluggify(pf2Action, { camel: 'dromedary' }) : '']
+            const visibility = pf2ShowDc ?? 'all'
+            if (pf2Action && action) {
+                action({
+                    event,
+                    glyph: pf2Glyph,
+                    variant: pf2Variant,
+                    difficultyClass: pf2Dc ? { scope: 'check', value: Number(pf2Dc) || 0, visibility } : undefined,
+                    skill: pf2Skill,
+                    actors: [actor],
+                })
+            } else {
+                console.warn(`PF2e System | Skip executing unknown action '${pf2Action}'`)
             }
-            default: {
-                const statistic = actor.getStatistic(pf2Check ?? '')
-                if (statistic) {
+        })
+    }
+
+    for (const link of links.filter(l => l.dataset.pf2Check && !l.dataset.invalid)) {
+        const { pf2Check, pf2Dc, pf2Traits, pf2Label, pf2Defense, pf2Adjustment, pf2Roller } = link.dataset
+        if (!pf2Check) return
+
+        link.addEventListener('click', event => {
+            const parsedTraits = pf2Traits
+                ?.split(',')
+                .map(trait => trait.trim())
+                .filter(trait => !!trait)
+            const eventRollParams = eventToRollParams(event)
+
+            switch (pf2Check) {
+                case 'flat': {
+                    const flatCheck = new actor.perception.constructor(actor, {
+                        label: '',
+                        slug: 'flat',
+                        modifiers: [],
+                        check: { type: 'flat-check' },
+                    })
+                    const dc = Number.isInteger(Number(pf2Dc)) ? { label: pf2Label, value: Number(pf2Dc) } : null
+                    flatCheck.roll({ ...eventRollParams, extraRollOptions: parsedTraits, dc })
+                    break
+                }
+                default: {
+                    const statistic = (() => {
+                        if (pf2Check in CONFIG.PF2E.magicTraditions) {
+                            const bestSpellcasting =
+                                actor.spellcasting
+                                    .filter(c => c.tradition === pf2Check)
+                                    .flatMap(s => s.statistic ?? [])
+                                    .sort((a, b) => b.check.mod - a.check.mod)
+                                    .shift() ?? null
+                            if (bestSpellcasting) return bestSpellcasting
+                        }
+                        return actor.getStatistic(pf2Check)
+                    })()
+                    if (!statistic) {
+                        console.warn(ErrorPF2e(`Skip rolling unknown statistic ${pf2Check}`).message)
+                        break
+                    }
+
+                    const targetActor = pf2Defense ? game.user.targets.first()?.actor : null
                     const dcValue = (() => {
                         const adjustment = Number(pf2Adjustment) || 0
                         if (pf2Dc === '@self.level') {
                             return calculateDC(actor.level) + adjustment
                         }
-                        return Number(pf2Dc) + adjustment
+                        return Number(pf2Dc ?? 'NaN') + adjustment
+                    })()
+                    const dc = (() => {
+                        if (Number.isInteger(dcValue)) {
+                            return { label: pf2Label, value: dcValue }
+                        } else if (pf2Defense) {
+                            const defenseStat = targetActor?.getStatistic(pf2Defense)
+                            return defenseStat
+                                ? {
+                                      statistic: defenseStat.dc,
+                                      scope: 'check',
+                                      value: defenseStat.dc.value,
+                                  }
+                                : null
+                        }
+                        return null
                     })()
 
-                    const dc = Number.isInteger(dcValue) ? { label: pf2Label, value: dcValue } : null
-
-                    statistic.check.roll({
+                    const isSavingThrow = tupleHasValue(['fortitude', 'reflex', 'will'], pf2Check)
+                    // Get actual traits and include as such
+                    const traits = isSavingThrow ? [] : parsedTraits?.filter(t => t in CONFIG.PF2E.actionTraits) ?? []
+                    statistic.roll({
                         ...eventRollParams,
                         extraRollOptions: parsedTraits,
-                        origin: actor,
+                        origin: isSavingThrow ? actor : null,
                         dc,
+                        target: !isSavingThrow && dc?.statistic ? targetActor : null,
+                        traits,
                     })
-                } else {
-                    console.warn(`PF2e System | Skip rolling unknown statistic ${pf2Check}`)
                 }
             }
-        }
-    })
+        })
+    }
 
-    $links.filter('[data-pf2-effect-area]').on('click', async event => {
-        const { pf2EffectArea, pf2Distance, pf2TemplateData, pf2Traits, pf2Width } = event.currentTarget.dataset
-        const templateConversion = {
-            burst: 'circle',
-            emanation: 'circle',
-            line: 'ray',
-            cone: 'cone',
-            rect: 'rect',
-        }
+    const templateConversion = {
+        burst: 'circle',
+        emanation: 'circle',
+        line: 'ray',
+        cone: 'cone',
+        rect: 'rect',
+    }
 
-        if (typeof pf2EffectArea === 'string') {
+    for (const link of links.filter(l => l.hasAttribute('data-pf2-effect-area'))) {
+        const { pf2EffectArea, pf2Distance, pf2TemplateData, pf2Traits, pf2Width } = link.dataset
+        link.addEventListener('click', () => {
+            if (typeof pf2EffectArea !== 'string') {
+                console.warn(`PF2e System | Could not create template'`)
+                return
+            }
+
             const templateData = JSON.parse(pf2TemplateData ?? '{}')
             templateData.distance ||= Number(pf2Distance)
             templateData.fillColor ||= game.user.color
@@ -212,9 +240,7 @@ export function listenInlineRoll($html, actor) {
             }
 
             const templateDoc = new CONFIG.MeasuredTemplate.documentClass(templateData, { parent: canvas.scene })
-            await new CONFIG.MeasuredTemplate.objectClass(templateDoc).drawPreview()
-        } else {
-            console.warn(`PF2e System | Could not create template'`)
-        }
-    })
+            new CONFIG.MeasuredTemplate.objectClass(templateDoc).drawPreview()
+        })
+    }
 }
