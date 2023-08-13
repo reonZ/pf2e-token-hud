3c13a95114a9b22c9bf23603b99f083451e76424
reonZ <651462+reonZ@users.noreply.github.com>
2023-08-13 13:18:03 +0200

    recall-knowledge is now a proper check

--- src/actions/recall-knowledge.js
+++ src/actions/recall-knowledge.js
@@ -5,21 +5,18 @@ import { getUniqueTarget, RANKS } from '../shared.js'
 const SKILLS = ['arcana', 'crafting', 'medicine', 'nature', 'occultism', 'religion', 'society']
 
 const SUCCESS = {
-    // 0: { icon: '<i class="fa-solid fa-circle-xmark"></i>', name: 'criticalFailure' },
     0: { icon: '<i class="fa-solid fa-xmark-large"></i><i class="fa-solid fa-xmark-large"></i>', name: 'criticalFailure' },
     1: { icon: '<i class="fa-solid fa-xmark-large"></i>', name: 'failure' },
     2: { icon: '<i class="fa-solid fa-check"></i>', name: 'success' },
     3: { icon: '<i class="fa-solid fa-check"></i><i class="fa-solid fa-check"></i>', name: 'criticalSuccess' },
-    // 3: { icon: '<i class="fa-solid fa-circle-check"></i>', name: 'criticalSuccess' },
 }
 
 export async function rollRecallKnowledges(actor) {
     const roll = await new Roll('1d20').evaluate({ async: true })
-    const result = roll.total
     const dieResult = roll.dice[0].total
     const dieSuccess = dieResult === 1 ? '0' : dieResult === 20 ? '3' : ''
     const lores = Object.values(actor.skills).filter(skill => skill.lore)
-    const target = getUniqueTarget(target => target.actor?.identificationDCs)
+    const target = getUniqueTarget(target => target.actor?.identificationDCs)?.actor
 
     let data = {
         dieSuccess,
@@ -29,7 +26,7 @@ export async function rollRecallKnowledges(actor) {
     }
 
     if (target) {
-        const { standard, skills, lore } = target.actor.identificationDCs
+        const { standard, skills, lore } = target.identificationDCs
 
         let skillsDCs = standard.progression.slice()
         skillsDCs.length = 4
@@ -42,41 +39,20 @@ export async function rollRecallKnowledges(actor) {
         })
 
         data.skillsDCs = skillsDCs
-        data.skills = skills.map(slug => {
-            const { mod, label, rank } = actor.skills[slug]
-            const total = result + mod
-
-            return {
-                label,
-                mod,
-                rank,
-                rankLabel: RANKS[rank],
-                total,
-                checks: skillsDCs.map(dc => {
-                    if (!dc) return `-`
-                    const success = calculateDegreeOfSuccess(total, dieResult, dc)
-                    return {
-                        success,
-                        icon: SUCCESS[success].icon,
-                        title: SUCCESS[success].name,
-                    }
-                }),
-            }
-        })
         data.loresDCs = loresDCs
-        data.lores = lores.map(({ label, rank, mod }) => ({
-            label,
-            rank,
-            mod,
-            modifier: modifier(mod),
-        }))
+
+        data.skills = await Promise.all(
+            skills.map(slug => {
+                const skill = actor.skills[slug]
+                return rollSkill(skill, dieResult, skillsDCs)
+            })
+        )
+
+        data.lores = await Promise.all(lores.map(lore => rollSkill(lore, dieResult)))
     } else {
-        data.skills = [...SKILLS.map(slug => actor.skills[slug]), ...lores].map(({ label, rank, mod }) => ({
-            label,
-            rank,
-            mod,
-            modifier: modifier(mod),
-        }))
+        data.skills = await Promise.all(
+            [...SKILLS.map(slug => actor.skills[slug]), ...lores].map(skill => rollSkill(skill, dieResult))
+        )
     }
 
     const flavor = await renderTemplate(templatePath('chat/recall-knowledge'), data)
@@ -87,4 +63,35 @@ export async function rollRecallKnowledges(actor) {
         rollMode: CONST.DICE_ROLL_MODES.BLIND,
         type: CONST.CHAT_MESSAGE_TYPES.ROLL,
     })
+}
+
+async function rollSkill(skill, dieResult, dcs) {
+    const { rank, label } = skill
+
+    const roll = await skill.roll({
+        createMessage: false,
+        skipDialog: true,
+        extraRollOptions: ['action:recall-knowledge'],
+    })
+
+    const mod = roll.total - roll.dice[0].total
+    const total = dieResult + mod
+
+    return {
+        mod,
+        rank,
+        label,
+        total,
+        modifier: modifier(mod),
+        rankLabel: RANKS[rank],
+        checks: dcs?.map(dc => {
+            if (!dc) return `-`
+            const success = calculateDegreeOfSuccess(total, dieResult, dc)
+            return {
+                success,
+                icon: SUCCESS[success].icon,
+                title: SUCCESS[success].name,
+            }
+        }),
+    }
 }
